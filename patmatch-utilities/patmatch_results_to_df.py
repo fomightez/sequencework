1c49b8959c9477f2599a0e511a8f8138cf0000d2
Wayne's Bioinformatics Code Portal <fomightez@gmail.com>
2021-01-25 14:45:31 -0500

    fix some unedited text left over from copy-paste

--- patmatch-utilities/patmatch_results_to_df.py
+++ patmatch-utilities/patmatch_results_to_df.py
@@ -117,7 +117,7 @@ prot_df
 ##################################
 #
 
-## Settings and options for output plot 
+## Settings and options for output dataframe-as-file 
 df_save_as_name = 'patmatch_pickled_df.pkl' # name for saving pickled dataframe
 
 #
